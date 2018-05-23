1ba1062aede6e51ee13d519cc63e74c27872d5b5
k_higashi <k_higashi@yumemi.co.jp>
2018-05-23 18:50:06 +0900

    fix: Prettier

--- config/bs-config.js
+++ config/bs-config.js
@@ -3,11 +3,11 @@ import historyApiFallback from 'connect-history-api-fallback';
 export default {
   files: './dist',
   // ローカル開発用
-  port: 8082,
-  open: false,
+  // port: 8082,
+  // open: false,
   // 他端末検証用
-  // port: 3000,
-  // open: 'external',
+  port: 3000,
+  open: 'external',
   notify: false,
   server: {
     baseDir: './dist',
